3f4e718dcf0193f1393c1cced85563b636244426
Ruslan Ponomarenko <ruponi@gmail.com>
2024-11-08 15:05:16 -0800

    Initial Commit

---
+++ Fetch_Mobile_Home_Recipe/Fetch_Mobile_Home_RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_Mobile_Home_RecipeApp.swift
+//  Fetch_Mobile_Home_Recipe
+//
+//  Created by Ruslan Ponomarenko on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_Mobile_Home_RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
